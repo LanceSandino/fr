1e546c3a54a70cb2d5dc4053f0339b372b87af3f
Lance Sandino <lance.sandino@gmail.com>
2020-03-24 10:47:22 -0500

    Update disk_hack.sh
    forgot sudo

--- disk_hack.sh
+++ disk_hack.sh
@@ -8,7 +8,7 @@ sudo mkfs.ext4 -m 0 -E lazy_itable_init=0,lazy_journal_init=0,discard /dev/$DISK
 sudo mkdir -p /data
 sudo mount -o discard,defaults /dev/$DISK /data
 #oops forgot to make it xfs -- don't forget to delete above!
-apt install xfsprogs -y && umount /data && mkfs.xfs -f -L XFS -b size=1024 /dev/$DISK && mount /dev/$DISK /data
+sudo apt install xfsprogs -y && umount /data && mkfs.xfs -f -L XFS -b size=1024 /dev/$DISK && mount /dev/$DISK /data
 echo ""
 echo "----------- DISKS -----------"
 echo "running 'df -h'"
